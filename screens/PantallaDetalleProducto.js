bb84e4210baabea14232af6d88f6da1ec5315f62
Martin Mata <martin.mata@grupoesfera.com.ar>
2021-01-13 12:10:10 -0300

    Agrega informacion a la pantalla de detalle de producto

--- screens/PantallaDetalleProducto.js
+++ screens/PantallaDetalleProducto.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, Button, ScrollView, Image } from 'react-native';
 import { useSelector } from 'react-redux';
+import Colores from '../constantes/Colores';
 
 const PantallaDetalleProducto = props => {
 
@@ -8,9 +9,14 @@ const PantallaDetalleProducto = props => {
     const productoSeleccionado = useSelector(estado => estado.productos.productosDisponibles.find(producto => producto.id === idProducto));
 
     return (
-        <View>
-            <Text>El detalle del producto {productoSeleccionado.titulo}</Text>
-        </View>
+        <ScrollView>
+            <Image style={styles.imagen} source={{uri: productoSeleccionado.urlImagen}} />
+            <View style={styles.botonera}>
+                <Button color={Colores.primario} style={styles.btnAgregarCarrito} title='Añadir al carrito' />
+            </View>
+            <Text style={styles.precio}>$ {productoSeleccionado.precio.toFixed(2)}</Text>
+            <Text style={styles.descripcion}>{productoSeleccionado.descripcion}</Text>
+        </ScrollView>
     );
 };
 
@@ -21,7 +27,28 @@ PantallaDetalleProducto.navigationOptions = navData => {
 }
 
 const styles = StyleSheet.create({
+    imagen: {
+        width: '100%',
+        height: 300
+    },
+    botonera: {
+        marginVertical: 10,
+        alignItems: 'center'
+    },
+    btnAgregarCarrito: {
 
+    },
+    precio: {
+        fontSize: 20,
+        color: '#888',
+        textAlign: 'center',
+        marginVertical: 20
+    },
+    descripcion: {
+        fontSize: 14,
+        textAlign: 'center',
+        marginHorizontal: 20
+    }
 });
 
 export default PantallaDetalleProducto;
